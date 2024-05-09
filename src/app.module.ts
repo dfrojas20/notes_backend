ae5921c2f9ab44546bbfe974a8a5d46bb5b2d1e0
dfrojas20 <123292688+dfrojas20@users.noreply.github.com>
2024-05-09 18:35:04 -0400

    agregada variable de entorno para las ips

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,25 @@ import { getNoteService } from './services/get.note.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Note, NoteSchema } from './schemas/note.schema';
 
+var url1: string = process.env.URL1
+var url2: string = process.env.URL2
+var url3: string = process.env.URL3
+var url: string = ''
+
+console.log(url1)
+console.log(url2)
+console.log(url3)
+
+if (url1 === null ){
+  url = 'mongodb://mongo:AyDxAggoIFCQkKBkKkYKFvrkjMOXmVNN@monorail.proxy.rlwy.net:52294'
+}else{
+  url = `mongodb://${url1}:27017,${url2}:27017,${url3}:27017/test?replicaSet=myReplicaSet`
+}
+
+console.log(url)
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo:AyDxAggoIFCQkKBkKkYKFvrkjMOXmVNN@monorail.proxy.rlwy.net:52294'),
+  imports: [MongooseModule.forRoot(url),
             MongooseModule.forFeature([{ name: Note.name, schema: NoteSchema }])         
   ],
   controllers: [AppController],
